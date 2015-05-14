247de47a0b7d52a1fe7eefb84f9ab85ba7e1daf7
Adrian Banks <github@adrianbanks.co.uk>
2015-05-14 21:35:09 +0100

    See if formatting applies to bot responses

--- SponsorshipBot/Commands/HelpCommand.cs
+++ SponsorshipBot/Commands/HelpCommand.cs
@@ -5,10 +5,10 @@
         public string Execute()
         {
             const string helpText = @"
-total                       Show the total amount pledged so far
-needed                      Set the total amount of sponsorship needed
-all                         Show all sponsors
-add <name> (amount)         Add a new sponsor
+`total                       `Show the total amount pledged so far
+`needed                      `Set the total amount of sponsorship needed
+`all                         `Show all sponsors
+`add <name> (amount)         `Add a new sponsor
 ";
             return helpText;
         }
