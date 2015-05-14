3524381a6493f582307437ee8e1df880b281df3b
Adrian Banks <github@adrianbanks.co.uk>
2015-05-14 21:10:14 +0100

    Better error handling

--- SponsorshipBot/Controllers/SlackBotController.cs
+++ SponsorshipBot/Controllers/SlackBotController.cs
@@ -29,7 +29,7 @@ namespace SponsorshipBot.Controllers
             }
             catch (Exception exception)
             {
-                return this.Request.CreateErrorResponse(HttpStatusCode.InternalServerError, exception);
+                return this.Request.CreateErrorResponse(HttpStatusCode.InternalServerError, exception.Message, exception);
             }
         }
 
