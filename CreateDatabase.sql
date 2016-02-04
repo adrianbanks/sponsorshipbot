ad7e9887b42fd17a15cc2775d3b9f508f38bb526
Adrian Banks <github@adrianbanks.co.uk>
2016-02-04 22:32:47 +0000

    Update the database creation script

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -2,22 +2,25 @@
 --CREATE USER [sponsorshipbot] FROM LOGIN [sponsorshipbot];
 --EXEC sp_addrolemember 'db_owner', 'sponsorshipbot';
 
+
 DROP TABLE [dbo].[Totals]
 GO
 DROP TABLE [dbo].[Sponsors]
 GO
+DROP TABLE [dbo].[Conferences]
+GO
 
-CREATE TABLE [dbo].[Totals]
+CREATE TABLE [dbo].[Conferences]
 (
 	[Id] [int] NOT NULL IDENTITY (1, 1),
-    [AmountNeeded] [decimal](7, 2) NOT NULL,
-    [StartingBalance] [decimal](7, 2) NOT NULL
+	[Name] [nvarchar](255) NOT NULL
 
-	CONSTRAINT [PK_Total] PRIMARY KEY CLUSTERED ([Id] ASC)
+	CONSTRAINT [PK_Conferences] PRIMARY KEY CLUSTERED ([Id] ASC)
 )
 GO
 
-INSERT INTO [dbo].[Totals] ([AmountNeeded], [StartingBalance]) VALUES (10000.00, 2500.00)
+INSERT INTO [dbo].[Conferences] ([Name]) VALUES ('DDDEA 2015')
+INSERT INTO [dbo].[Conferences] ([Name]) VALUES ('DDDEA 2016')
 GO
 
 CREATE TABLE [dbo].[Sponsors]
@@ -26,8 +29,28 @@ CREATE TABLE [dbo].[Sponsors]
 	[Name] [nvarchar](255) NOT NULL,
     [AmountPledged] [decimal](7, 2) NULL,
     [AmountReceived] [decimal](7, 2) NULL,
-	[AddedBy] [nchar](255) NOT NULL
+	[AddedBy] [nchar](255) NOT NULL,
+	[ConferenceId] [int] NOT NULL
 
 	CONSTRAINT [PK_Sponsors] PRIMARY KEY CLUSTERED ([Id] ASC)
 )
+GO
+
+ALTER TABLE [dbo].[Sponsors] ADD CONSTRAINT [FK_Sponsors_Conference] FOREIGN KEY([ConferenceId])
+	REFERENCES [dbo].[Conferences] ([Id])
+GO
+
+CREATE TABLE [dbo].[Totals]
+(
+	[Id] [int] NOT NULL IDENTITY (1, 1),
+    [AmountNeeded] [decimal](7, 2) NOT NULL,
+    [StartingBalance] [decimal](7, 2) NOT NULL,
+	[ConferenceId] [int] NOT NULL
+
+	CONSTRAINT [PK_Totals] PRIMARY KEY CLUSTERED ([Id] ASC)
+)
+GO
+
+ALTER TABLE [dbo].[Sponsors] ADD CONSTRAINT [FK_Totals_Conference] FOREIGN KEY([ConferenceId])
+	REFERENCES [dbo].[Conferences] ([Id])
 GO
